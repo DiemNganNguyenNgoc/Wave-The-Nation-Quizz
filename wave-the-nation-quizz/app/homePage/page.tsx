0a071ba8374a98f1a6366258f9f55fc8b3daea43
DiemNganNguyenNgoc <22520933@gm.uit.edu.vn>
2025-06-17 11:54:28 +0700

    implement_home page

--- wave-the-nation-quizz/app/homePage/page.tsx
+++ wave-the-nation-quizz/app/homePage/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { useRouter } from "next/navigation";
+import ButtonComponent from "@/component/ButtonComponent/buttonComponent";
 export default function HomePage() {
   const router= useRouter();
 
@@ -13,10 +14,7 @@ export default function HomePage() {
         </h1>
 
         {/* Nút start */}
-        <button className="hover:cursor-pointer rounded-md bg-blue-500 px-6 py-2 text-lg font-semibold text-white shadow hover:bg-blue-600 focus:outline-none focus:ring-4 focus:ring-blue-300"
-        onClick={()=> router.push('/start')}>
-          Start
-        </button>
+       <ButtonComponent text="Start"></ButtonComponent>
       </div>
     </div>
   );
